895a4cf0ccec15f6e989abbc42c64af26a2fbcf2
satwik2711 <satwik3.14159@gmail.com>
2024-03-28 18:52:14 +0530

    vector embedding

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 # for pdfs to vector embeddings 
 from langchain.prompts import PromptTemplate
-
+from langchain.vectorstores import FAISS
 from dotenv import load_dotenv
 from langchain.chains.question_answering import load_qa_chain
 import google.generativeai as genai
@@ -22,3 +22,15 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text+= page.extract_text()
     return  text
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
