46ec2bcf03044564ba031809c23ebd579da9c6f3
satwik2711 <satwik3.14159@gmail.com>
2024-03-28 18:47:47 +0530

    reading the texts

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+# for pdfs to vector embeddings 
+from langchain.prompts import PromptTemplate
+
+from dotenv import load_dotenv
+from langchain.chains.question_answering import load_qa_chain
+import google.generativeai as genai
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
